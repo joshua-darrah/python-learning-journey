8e9a9c55f0dc6ccbdfeed4ec4438c37719b0dc64
Joshua Darrah <darrahjoshua551@gmail.com>
2025-10-29 13:42:20 +0000

    Created expense_tracker Day 8 - File Handling

---
+++ Week_2/expense_tracker.py
@@ -0,0 +1,39 @@
+import csv
+
+def add_expense():
+    date = input("Enter date (YYYY-MM-DD): ")
+    category = input("Enter category (e.g. Food, Transport): ")
+    amount = input("Enter amount (GHS): ")
+
+    with open("expenses.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([date, category, amount])
+    print("Expense added successfully!")
+
+def view_expenses():
+    print("\n--- Your Expenses ---")
+    with open("expenses.csv", "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(row)
+
+def main():
+    while True:
+        print("\n=== Simple Expense Tracker ===")
+        print("1. Add Expense")
+        print("2. View Expenses")
+        print("3. Exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            add_expense()
+        elif choice == "2":
+            view_expenses()
+        elif choice == "3":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Try again.")
+
+main()
